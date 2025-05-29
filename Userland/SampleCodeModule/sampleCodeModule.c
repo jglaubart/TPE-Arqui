e409b018df81e04553bd3ddf1f25189730fe6e4c
jglaubart <jglaubart@itba.edu.ar>
2025-05-29 15:52:44 -0300

    tries

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -3,34 +3,10 @@
 
 int main() {
 	puts("Hello World\bem!\n");
-	puts("pampu\n\n\n\n");
+	puts("pampu\n");
 	puts("ahorita la andaba yo buscando, pero no la encontre, busque entre mil botellas, pero no la encontre, me dicen el que la encuentra la va a tomar y la va a beber y la va a beber y la va a beber\b\n");
-	puts("\n\n\n\n\n\n\n\n");
-	puts("hola1\n");
-	puts("\n\n\n\n\n\n\n\n");
-	puts("hola2\n");
-	puts("\n\n\n\n\n\n\n\n");
-	puts("hola3\n");
-	puts("\n\n\n\n\n\n\n\n");
-	puts("hola4\n");
-	puts("\n\n\n\n\n\n\n\n");
-	puts("hola5\n");
-	puts("\n\n\n\n\n\n\n\n");
-	puts("hola6\n");
-	puts("\n\n\n\n\n\n\n\n");
-	puts("hola7\n");
-	puts("\n\n\n\n\n\n\n\n");
-	puts("hola8\n");
-	puts("hola8\n");
-	puts("hola8\n");
-	puts("hola8\n");
-	puts("hola8\n");
-	puts("hola8\n");
-	puts("hola8\n");
-	puts("hola8\n");
-	puts("hola8\n");
-	puts("hola8\n");
-	puts("hola8\n");
+	newLine();
+	putChar('a');
 
 	return 0;
 }
