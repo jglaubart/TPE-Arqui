f03eb0db4aa9386706a521757f0a8ed992c9f3ee
eugemigliaro <eugenio.migliaro@gmail.com>
2025-05-29 19:02:37 -0300

    empezado el keyboardDriver

--- Kernel/include/syscall_handler.h
+++ Kernel/include/syscall_handler.h
@@ -15,5 +15,6 @@ typedef enum{
 uint64_t syscall_handler(uint64_t rax, uint64_t rdi, uint64_t rsi, uint64_t rdx, uint64_t r10, uint64_t r8, uint64_t r9);
 
 uint64_t sys_write(uint64_t fd, const char *buf, uint64_t count);
+uint64_t sys_read(uint64_t fd, char *buf, uint64_t count);
 
 #endif
