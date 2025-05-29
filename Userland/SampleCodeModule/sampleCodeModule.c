b0c4001a7b5e2ddbddcfdf10b51d509115bdae51
jglaubart <jglaubart@itba.edu.ar>
2025-05-29 16:28:24 -0300

    a

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -3,10 +3,12 @@
 
 int main() {
 	puts("Hello World\bem!\n");
-	puts("pampu\n");
-	puts("ahorita la andaba yo buscando, pero no la encontre, busque entre mil botellas, pero no la encontre, me dicen el que la encuentra la va a tomar y la va a beber y la va a beber y la va a beber\b\n");
-	newLine();
 	putChar('a');
+	puts("ahorita la andaba yo buscando, pero no la encontre, busque entre mil botellas, pero no la encontre, me dicen el que la encuentra la va a tomar y la va a beber y la va a beber y la va a beber\b\n");
+	for(int i=0; i<25; i++){
+		newLine();
+	}
+	puts("prueba de auto-scroll\n");
 
 	return 0;
 }
